5502bf0c4427558509f21d1c8418e9bac94b6b69
abruti <1065848582@qq.com>
2024-04-15 15:52:16 +0800

    解析数据包

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -2,6 +2,77 @@
 #include "pch.h"
 #include "framework.h"
 
+#define BUFFER_SIZE 4096
+class CPacket {
+public:
+	CPacket() :sHead(0), nLength(0), sCmd(0), sSUm(0) {}
+	CPacket(const CPacket& pack) {
+		sHead = pack.sHead;
+		nLength = pack.nLength;
+		sCmd = pack.sCmd;
+		strData = pack.strData;
+		sSUm = pack.sSUm;
+	}
+	CPacket& operator=(const CPacket& pack) {
+		if (this != &pack) {
+			sHead = pack.sHead;
+			nLength = pack.nLength;
+			sCmd = pack.sCmd;
+			strData = pack.strData;
+			sSUm = pack.sSUm;
+		}
+		return *this;
+	}
+	CPacket(const BYTE* pData, size_t& nSize) {
+		size_t i = 0;	// i始终指向已读到数据最新的位置
+		for (; i < nSize; i++) {
+			if ( *(WORD*)(pData + i) == 0xFEFF ) {
+				sHead = *(WORD*)(pData + i); // 找到包头
+				i += 2;	// 防止只有包头FEFF，占两个字节，但没有数据
+				break;
+			}
+		}
+		if (i+4+2+2 > nSize) {// 包数据可能不全，或包头未全部接收到，解析失败  +nLength +sCmd +sSum
+			nSize = 0;
+			return;
+		}
+
+		nLength = *(DWORD*)(pData + i);
+		i += 4;
+		if (nLength + i > nSize) { // 包没有完全接收到，比如只收到一半，解析失败
+			nSize = 0;
+			return;
+		}
+
+		sCmd = *(WORD*)(pData + i);
+		i += 2;
+		if (nLength > 4) {
+			strData.resize(nLength - 2 - 2); // 减去sCmd和sSum的长度
+			memcpy((void*)strData.c_str(), pData + i, nLength - 4); //c_str()把string 对象转换成c中的字符串样式
+			i += nLength - 4;
+		}
+
+		sSUm = *(WORD*)(pData + i);
+		i += 2;
+		WORD sum = 0;
+		for (size_t j = 0; j < strData.size(); j++) { // 进行和校验
+			sum += BYTE(strData[i]) & 0xFF;
+		}
+		if (sum == sSUm) {
+			nSize = i; //  head(2字节）nLength(4字节)data...
+			return;
+		}
+		nSize = 0;
+	}
+	~CPacket() {}
+public:
+	WORD sHead;				// 包头，固定位，0xFEFF
+	DWORD nLength;			// 包长度，从控制命令开始，到和校验结束
+	WORD sCmd;				// 控制命令
+	std::string strData;	// 包数据
+	WORD sSUm;				// 和校验
+};
+
 class CServerSocket
 {
 public:
@@ -28,15 +99,26 @@ public:
 		int client_sz = sizeof(client_addr);
 		m_client = accept(m_sock, (sockaddr*)&client_addr, &client_sz);
 		if (m_client == -1) return FALSE;
-		return TRUE;	
+		return TRUE;
 	}
 	int DealCommand() {
-		if (m_client == -1) return 0;
-		char buffer[1024] = "";
-		while (true){
-			int len = recv(m_client, buffer, sizeof(buffer), 0);
+		if (m_client == -1) return -1;
+		char* buffer = new char[BUFFER_SIZE];
+		memset(buffer, 0, BUFFER_SIZE);
+		size_t index = 0;
+		while (true) {
+			size_t len = recv(m_client, buffer+index, BUFFER_SIZE -index, 0);
 			if (len <= 0) return -1;
+			index += len;
+			len = index;
+			m_packet = CPacket((BYTE*)buffer, len);
+			if (len > 0) {
+				memmove(buffer, buffer + len, BUFFER_SIZE - len);
+				index -= len;
+				return m_packet.sCmd;
+			}
 		}
+		return -1;
 	}
 	BOOL Send(const char* pData, int nSize) {
 		if (m_client == -1) return FALSE;
@@ -44,6 +126,7 @@ public:
 	}
 private:
 	SOCKET m_sock, m_client;
+	CPacket m_packet;
 	// 单例模式，保证整个系统周期内只产生一个实例，所以将构造和析构设为私有，禁止外部构造和析构
 	// 将构造和析构函数设为私有，避免外部控制
 	CServerSocket& operator=(const CServerSocket&& ss) {}
@@ -67,7 +150,7 @@ private:
 		// 套接字初始化
 		WSADATA data;
 		if (WSAStartup(MAKEWORD(1, 1), &data) != 0) {
-			return FALSE; 
+			return FALSE;
 		}
 		else {
 			m_sock = socket(PF_INET, SOCK_STREAM, 0);
